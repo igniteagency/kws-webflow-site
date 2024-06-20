09885accec4f11b65ab877fea89919da7dce46d9
Paras Shah <parasshah195@gmail.com>
2024-06-20 10:28:25 +0530

    Install and add GSAP globally

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,10 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import { animatedDetailsAccordions } from './components/accordions';
+
+window.gsap = gsap;
+window.gsap.registerPlugin(ScrollTrigger);
 
 window.Webflow?.push(() => {
   animatedDetailsAccordions();
