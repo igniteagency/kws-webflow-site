1ae64f5f51ebe53e0c8ada6d6d9cf1d2237ccbac
Paras Shah <parasshah195@gmail.com>
2024-06-24 18:32:59 +0530

    Set alias for components folder

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,7 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-import { animatedDetailsAccordions } from './components/accordions';
+import { animatedDetailsAccordions } from '$components/accordions';
 
 window.gsap = gsap;
 window.gsap.registerPlugin(ScrollTrigger);
