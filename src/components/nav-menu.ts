cd6e3ab11ef6d2b43ebfa509764a959718c1171e
Paras Shah <parasshah195@gmail.com>
2024-06-24 18:33:18 +0530

    Add nav menu accordion interaction

---
+++ src/components/nav-menu.ts
@@ -0,0 +1,66 @@
+/** Set [data-el="nav-menu-item"] on each accordion trigger */
+
+const ITEM_SELECTOR = '[data-el="nav-menu-item"]';
+const TOGGLE_SELECTOR = 'summary';
+const CONTENT_SELECTOR = 'summary + div';
+
+const ANIMATION_DURATION_IN_MS = 300;
+
+export function initNavMenuAccordions() {
+  const accordionsList = document.querySelectorAll<HTMLDetailsElement>(ITEM_SELECTOR);
+
+  const isTabletAndBelow = window.matchMedia('max-width: 991px');
+
+  accordionsList.forEach((accordionEl) => {
+    const accordionToggleEl = accordionEl.querySelector(TOGGLE_SELECTOR);
+    const accordionContentEl = accordionEl.querySelector(CONTENT_SELECTOR);
+
+    if (!accordionToggleEl || !accordionContentEl) {
+      console.error('Accordion toggle or content not found', accordionToggleEl, accordionContentEl);
+      return;
+    }
+
+    if (!isTabletAndBelow) {
+      return;
+    }
+
+    /** Height open animation on tablet and below screens */
+    accordionToggleEl.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const isOpening = !accordionEl.open;
+
+      if (isOpening) {
+        const height = accordionContentEl.scrollHeight;
+        accordionContentEl.style.height = '0px';
+        accordionEl.open = true;
+        accordionContentEl.animate([{ height: '0px' }, { height: `${height}px` }], {
+          duration: ANIMATION_DURATION_IN_MS,
+          fill: 'forwards',
+        }).onfinish = () => {
+          accordionContentEl.style.height = 'auto';
+        };
+
+        accordionsList.forEach((otherAccordion) => {
+          if (otherAccordion !== accordionEl && otherAccordion.open) {
+            otherAccordion.querySelector(TOGGLE_SELECTOR)?.click();
+          }
+        });
+      } else {
+        const height = accordionContentEl.scrollHeight;
+        const animation = accordionContentEl.animate(
+          [{ height: `${height}px` }, { height: '0px' }],
+          {
+            duration: ANIMATION_DURATION_IN_MS,
+            fill: 'forwards',
+          }
+        );
+
+        animation.onfinish = () => {
+          accordionEl.open = false;
+          accordionContentEl.style.height = '';
+        };
+      }
+    });
+  });
+}
--- src/global.ts
+++ src/global.ts
@@ -2,10 +2,12 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import { animatedDetailsAccordions } from '$components/accordions';
+import { initNavMenuAccordions } from '$components/nav-menu';
 
 window.gsap = gsap;
 window.gsap.registerPlugin(ScrollTrigger);
 
 window.Webflow?.push(() => {
+  initNavMenuAccordions();
   animatedDetailsAccordions();
 });
