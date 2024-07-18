78f52c044fd07197ad0b3317d311b32542092589
Paras Shah <parasshah195@gmail.com>
2024-07-18 12:02:24 +0530

    Move tabs autoplay functionality from home to global
    Add default interval

--- src/global.ts
+++ src/global.ts
@@ -4,6 +4,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { animatedDetailsAccordions } from '$components/accordions';
 import { initBGColorChange } from '$components/bg-color-change';
 import { initNavScripts } from '$components/nav';
+import { TabAutoplay } from '$components/tabs-autoplay';
 
 window.gsap = gsap;
 window.ScrollTrigger = ScrollTrigger;
@@ -13,4 +14,25 @@ window.Webflow?.push(() => {
   initNavScripts();
   initBGColorChange();
   animatedDetailsAccordions();
+  initTabAutoplay();
 });
+
+function initTabAutoplay() {
+  const TAB_COMPONENT_SELECTOR = '[data-el="tabs-component"]';
+  const TAB_ITEM_DESCRIPTION_WRAPPER_SELECTOR = '[data-el="tab-item-description"]';
+  const AUTOPLAY_INTERVAL_ATTRIBUTE = 'data-tabs-autoplay-interval';
+
+  const tabComponentEl = document.querySelector(TAB_COMPONENT_SELECTOR);
+  if (!tabComponentEl) {
+    // console.debug('Tabs component not found', { tabComponentEl });
+    return;
+  }
+
+  let interval = 5000;
+
+  if (tabComponentEl.hasAttribute(AUTOPLAY_INTERVAL_ATTRIBUTE)) {
+    interval = parseInt(tabComponentEl.getAttribute(AUTOPLAY_INTERVAL_ATTRIBUTE)!, 10);
+  }
+
+  new TabAutoplay(tabComponentEl, interval, TAB_ITEM_DESCRIPTION_WRAPPER_SELECTOR);
+}
