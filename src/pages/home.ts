c16f9703582e59241ddfe6452288b38a161eb738
Paras Shah <parasshah195@gmail.com>
2024-07-31 17:44:53 +0530

    Update number counter to be an independent component

--- src/pages/home.ts
+++
@@ -1,7 +0,0 @@
-import { SCRIPTS_LOADED_EVENT } from 'src/constants';
-
-import { initNumberCounter } from '$components/number-counter';
-
-window.addEventListener(SCRIPTS_LOADED_EVENT, () => {
-  initNumberCounter();
-});
