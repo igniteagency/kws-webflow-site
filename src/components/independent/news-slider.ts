acac2e3550e935c9483b41b7279dafbdec081fb7
Paras Shah <parasshah195@gmail.com>
2024-09-13 15:48:21 +0530

    Error handling on news slider initialisation
    which can fail if the news slider section is invisible or the CMS is empty with so slide data

--- src/components/independent/news-slider.ts
+++ src/components/independent/news-slider.ts
@@ -15,33 +15,38 @@ function initNewsSlider() {
     return;
   }
 
-  new Swiper(componentEl, {
-    modules: [Navigation, Pagination, EffectCreative, A11y],
-    effect: 'creative',
-    loop: true,
-    loopAddBlankSlides: true,
-    slidesPerView: 'auto',
-    spaceBetween: 32,
-    creativeEffect: {
-      perspective: true,
-      prev: {
-        scale: 0.9,
-        translate: [0, 12, -1], // Move previous slides to the left
-        opacity: 0.66,
-        origin: 'bottom center',
+  try {
+    new Swiper(componentEl, {
+      modules: [Navigation, Pagination, EffectCreative, A11y],
+      effect: 'creative',
+      loop: true,
+      loopAddBlankSlides: true,
+      slidesPerView: 'auto',
+      spaceBetween: 32,
+      creativeEffect: {
+        perspective: true,
+        prev: {
+          scale: 0.9,
+          translate: [0, 12, -1], // Move previous slides to the left
+          opacity: 0.66,
+          origin: 'bottom center',
+        },
+        next: {
+          translate: ['108%', 0, 0], // Move next slides to the right
+        },
+        limitProgress: 8,
       },
-      next: {
-        translate: ['108%', 0, 0], // Move next slides to the right
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
       },
-      limitProgress: 8,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  } catch (e) {
+    console.warn('Error initialising the news slider', { e }, 'tried to initialise:', componentEl);
+    return;
+  }
 }
