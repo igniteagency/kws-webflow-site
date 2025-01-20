fed3ba0278746b455b755682016658f3de78f714
Paras Shah <parasshah195@gmail.com>
2025-01-21 00:58:03 +0530

    Add auto minimise functionality to announcement dialog
    when half hero has scrolled past the viewport
    
    Also refactor the structure to facilitate this update

--- src/components/announcements.ts
+++ src/components/announcements.ts
@@ -1,25 +1,119 @@
 import Cookies from 'js-cookie';
 
 export function announcementTracker() {
-  const announcementItemsList = document.querySelectorAll('[data-announcement-slug]');
+  const ANNOUNCEMENT_COMPONENT_SELECTOR = 'dialog[data-el="announcement-item"]';
+  const ANNOUNCEMENT_BAR_LIST_WRAPPER_SELECTOR = '.announcement-bar_list-wrapper';
+  const ANNOUNCEMENT_BAR_NOTICE_ICON_SELECTOR = '.announcement-bar_notice-icon';
 
-  announcementItemsList.forEach((itemEl) => {
-    const slug = itemEl.dataset.announcementSlug;
-    const itemDialogEl = itemEl.querySelector<HTMLDialogElement>(
-      'dialog[data-el="announcement-item"]'
-    );
+  const dialogComponentEl = document.querySelector<HTMLDialogElement>(
+    ANNOUNCEMENT_COMPONENT_SELECTOR
+  );
+  const heroSection = document.querySelector('.section_hero, .section_hero-text');
 
-    if (!itemDialogEl) {
-      return;
+  const DIALOG_ANIMATION_DURATION_SECONDS = 0.5;
+  const DIALOG_CONTENT_ANIMATION_DURATION_SECONDS = 0.25;
+
+  if (!dialogComponentEl) {
+    return;
+  }
+
+  let dialogHeight = dialogComponentEl.clientHeight;
+  let dialogWidth = dialogComponentEl.clientWidth;
+
+  if (Cookies.get(`announcement-closed`) === 'true') {
+    console.debug(`Announcement item already closed`);
+    dialogComponentEl.close();
+    return;
+  }
+
+  let isCollapsed = false;
+
+  // Create intersection observer
+  if (heroSection) {
+    window.gsap.to(heroSection, {
+      scrollTrigger: {
+        trigger: heroSection,
+        start: 'center top',
+        onEnter: () => {
+          if (!isCollapsed) {
+            isCollapsed = true;
+            animateDialog(isCollapsed);
+          }
+        },
+        onLeaveBack: () => {
+          if (isCollapsed) {
+            isCollapsed = false;
+            animateDialog(isCollapsed);
+          }
+        },
+      },
+    });
+  }
+
+  dialogComponentEl.addEventListener('click', () => {
+    if (isCollapsed) {
+      isCollapsed = false;
+      animateDialog(isCollapsed);
     }
+  });
 
-    if (Cookies.get(`announcement-${slug}-closed`) === 'true') {
-      console.debug(`Announcement item '${slug}' already closed`);
-      itemEl.classList.add('hide');
-    }
-
-    itemDialogEl?.addEventListener('close', () => {
-      Cookies.set(`announcement-${slug}-closed`, 'true', { expires: 30 });
+  dialogComponentEl.addEventListener('close', () => {
+    window.gsap.killTweensOf(heroSection); // Remove scrolltrigger when announcement is closed
+    window.gsap.killTweensOf(dialogComponentEl);
+    Cookies.set(`announcement-closed`, 'true', {
+      expires: 30,
+      sameSite: 'None',
+      secure: true,
     });
   });
+
+  function animateDialog(isCollapsed: boolean) {
+    if (isCollapsed) {
+      window.gsap.to(dialogComponentEl, {
+        duration: DIALOG_ANIMATION_DURATION_SECONDS,
+        width: '3.5rem',
+        height: '3.5rem',
+        borderRadius: '50%',
+        onStart: () => {
+          const tl = window.gsap.timeline();
+          tl.to(ANNOUNCEMENT_BAR_LIST_WRAPPER_SELECTOR, {
+            duration: DIALOG_CONTENT_ANIMATION_DURATION_SECONDS,
+            opacity: 0,
+            display: 'none',
+          });
+          tl.to(ANNOUNCEMENT_BAR_NOTICE_ICON_SELECTOR, {
+            duration: DIALOG_CONTENT_ANIMATION_DURATION_SECONDS,
+            opacity: 1,
+            display: 'flex',
+          });
+          tl.eventCallback('onComplete', () => {
+            tl.kill();
+          });
+        },
+      });
+    } else {
+      window.gsap.to(dialogComponentEl, {
+        duration: DIALOG_ANIMATION_DURATION_SECONDS,
+        width: dialogWidth,
+        height: dialogHeight,
+        borderRadius: '0',
+        onStart: () => {
+          const tl = window.gsap.timeline();
+          tl.to(ANNOUNCEMENT_BAR_NOTICE_ICON_SELECTOR, {
+            duration: DIALOG_CONTENT_ANIMATION_DURATION_SECONDS,
+            opacity: 0,
+            display: 'none',
+          });
+          tl.to(ANNOUNCEMENT_BAR_LIST_WRAPPER_SELECTOR, {
+            duration: DIALOG_CONTENT_ANIMATION_DURATION_SECONDS,
+            opacity: 1,
+            display: 'block',
+          });
+          tl.eventCallback('onComplete', () => {
+            tl.kill();
+          });
+        },
+      });
+    }
+  }
 }
